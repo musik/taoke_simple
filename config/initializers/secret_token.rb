1d59289b04d6c5f3085d6cde45ea6f5ca248f7a3
musik <860577@gmail.com>
2012-12-29 20:51:37 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaokeSimple::Application.config.secret_token = '3ec052c074e286cded6372ea378da610361246b40f23164cf12e5d38b841d67c10ea0ceae6ecc71749ba673a36358221ecd7e67a18d1c6dedbe1e6a961588c67'
