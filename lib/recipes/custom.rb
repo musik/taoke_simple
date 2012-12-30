6f7617a9cb13551a5efd156bab22fd65bc002ce7
musik <860577@gmail.com>
2012-12-30 22:33:09 +0800

    sphinx

---
+++ lib/recipes/custom.rb
@@ -0,0 +1,34 @@
+require 'erb'
+
+Capistrano::Configuration.instance.load do
+  namespace :app do
+    desc "|Custom| Create database.yml in shared path with settings for current stage and test env"
+    task :yml do      
+      #run "if [ ! -d '#{shared_path}/config' ]; then mkdir -p #{shared_path}/config; fi;"
+      #run "rm -rf #{shared_path}/config/database.yml"
+      #upload './config/database.yml', "#{shared_path}/config/database.yml"
+      upload './config/application.yml', "#{shared_path}/config/application.yml"
+    end 
+  end
+  namespace :unicorn do
+    task :symlink do
+      run "rm -rf #{current_path}/config/unicorn.rb && ln -nfs #{shared_path}/config/unicorn.rb #{current_path}/config/unicorn.rb"
+    end
+  end
+  namespace :nginx do
+    #task :reload, :roles => :app , :except => { :no_release => true } do
+      #sudo "service nginx reload"
+    #end
+  end
+  namespace :ss do
+    task :uptime,:roles => :app do
+      run "uptime"
+    end
+    task :bundle, :roles => :app do
+      run "which bundle"
+    end
+    task :update_rates, :roles => :app do
+      run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec rails runner 'Shop::Import.new.update_all_rates'"
+    end
+  end
+end
